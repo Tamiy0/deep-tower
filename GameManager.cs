9f6d9c6acd14425f6b3e60e4969e18b1328264fd
Emile <ebonnet008@ensc.fr>
2026-04-14 21:53:25 +0200

    Création menu

--- GameManager.cs
+++ GameManager.cs
@@ -3,14 +3,14 @@ using System;
 
 public partial class GameManager : Node
 {
-    public void EnregistrerMonstre(MonstreDeBase monstre)
-    {
-        monstre.JoueurAttrape += GameOver;
-        GD.Print($"Monstre {monstre.Name} enregistré par le GameManager");
-    }
+	public void EnregistrerMonstre(MonstreDeBase monstre)
+	{
+		monstre.JoueurAttrape += GameOver;
+		GD.Print($"Monstre {monstre.Name} enregistré par le GameManager");
+	}
 
-    private void GameOver()
-    {
-        GD.Print("Le joueur a été attrapé ! Condition de défaite activée.");
-    }
+	private void GameOver()
+	{
+		GD.Print("Le joueur a été attrapé ! Condition de défaite activée.");
+	}
 }
